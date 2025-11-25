e5cd12036a7ab810124c0bddad1d02c72f46e88f
wesleyraads <wesley.amorim@ratoeiraads.com>
2025-11-25 19:30:16 -0300

    ajuste

--- src/server/server.ts
+++ src/server/server.ts
@@ -22,7 +22,7 @@ process.on('exit', (code) => {
 });
 
 
-cron.schedule('0 0 0,1,2,3,4,5,6,7,8,9 * * *', () => {
+cron.schedule('0 */30 0,1,2,3,4,5,6,7,8,9 * * *', () => {
     console.log('Executando cronjob nos horários 12h, 13h, 14h e 15h...');
     LimpezaDaseDadosService.deletarDadosLinkVisitaAcessoParametros();
 });
